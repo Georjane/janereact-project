507925e0e17712ed4bf3e64d4ffceb73c7b00ff3
Georjane <pzgeorjane@gmail.com>
2021-07-10 10:22:30 +0100

    Used styled-components fr styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const RedText = styled.h1`
+  color: green;
+`;
 
 const Home = () => {
   const styles = {
@@ -6,11 +11,13 @@ const Home = () => {
   };
   return (
     <div>
+      <RedText>Hey Jane</RedText>
       <h1>Hello from Home!!!</h1>
       {/* inline styling */}
       <p style={{ color: 'red' }}>
         Example Text
       </p>
+      {/* object in our code that stores the style properties */}
       <p style={styles}>
         Example Text
       </p>
